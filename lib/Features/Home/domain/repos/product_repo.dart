567af743cead302758338ae4be19790122cbd293
Pola Maged <74374673+polamaged@users.noreply.github.com>
2025-10-20 02:40:29 +0300

    Update product_repo.dart

--- lib/Features/Home/domain/repos/product_repo.dart
+++ lib/Features/Home/domain/repos/product_repo.dart
@@ -6,15 +6,12 @@ import 'package:otex_app_task/Features/Home/data/models/product_model.dart';
 
 
 abstract class ProductRepo {
-  // جلب كل المنتجات
+  
   Future<Either<Failure, List<ProductModel>>> fetchAllProducts();
 
-  // جلب التصنيفات
   Future<Either<Failure, List<CategoryModel>>> fetchCategories();
 
-  // جلب المنتجات حسب الكاتيجوري
   Future<Either<Failure, List<ProductModel>>> fetchProductsByCategory(int categoryId);
 
-  // جلب منتج محدد حسب id
   Future<Either<Failure, ProductModel>> fetchProductById(int productId);
 }
